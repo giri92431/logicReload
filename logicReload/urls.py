c098981fd53569980ef0c5a724bd7fca23ff020b
Girish Kumar P <Girish.Kumar@euroindia.euromonitor.local>
2017-04-03 18:17:11 +0530

    home and blog app created

--- logicReload/urls.py
+++ logicReload/urls.py
@@ -14,7 +14,10 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
-from logicReload.views import hello
+
 urlpatterns = [
+	url(r'^$',include('home.urls')),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^blog/',include('blog.urls')),
+    
 ]
